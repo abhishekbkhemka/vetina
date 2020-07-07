d032eee40572ff6d05ecb01a74ba9aed6868f94f
abhi <abhishekbkhemka@gmail.com>
2020-07-07 19:10:10 +0530

    fixes

--- sendMail.php
+++ sendMail.php
@@ -16,10 +16,25 @@ $email->setFrom("abhi@rikoouu.com", "Example User");
 $email->setSubject("Sample reques from nn ");
 $email->addTo("abhishekbkhemka@gmail.com", "Example User");
 $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$mobileNo = $_POST['mobileNo'];
+$city = $_POST['city'];
+$email = $_POST['email'];
+$postal: $_POST['#postal'];
+$message = $_POST['message'];
+$formcontent=" From: Website Contact Form $email \n
+		Name: $fname\n
+        Mobile Number: $mobileNo \n
+        City: $city \n
+        Email Address: $email \n
+        Postal Code: $postal \n
+		Message: $message";
 $email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+    "text/html", $formcontent
 );
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+//$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+$sendgrid = new \SendGrid('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');
 try {
     $response = $sendgrid->send($email);
      echo json_encode($response->body());
